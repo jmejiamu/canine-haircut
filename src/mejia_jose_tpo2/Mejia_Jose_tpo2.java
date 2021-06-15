018cbec52b6ecba992e3ad071d6b993bfac8f598
Jose <jmejiamu@gmail.com>
2021-06-14 23:02:10 -0700

    first commit

---
+++ src/mejia_jose_tpo2/Mejia_Jose_tpo2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mejia_jose_tpo2;
+
+/**
+ *
+ * @author joch_
+ */
+public class Mejia_Jose_tpo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
